ec270d25dee30acb44294ad96d066725e85355d2
Osman Samad <osamad84@yahoo.com>
2020-01-24 16:59:12 -0600

    week 1 day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+}
+ngOnInit() {
+  this.todoTitle = '';
+  this.todoList = [
+    // example of how to make an item in todo list
+    { title: 'Install Angular CLI', isDone: false },
+  ];
+}
+// adds a todo to our list
+addTodo():void {
+  this.todoList.push({
+    title: this.todoTitle,
+    isDone: false
+  });
+  
+  // resets our todoTitle variable to an empty string
+  this.todoTitle = '';
+}
+deleteTodo(todo:any) {
+  const index = this.todoList.findIndex(todoItem => todoItem === todo);
+  this.todoList.splice(index, 1);
 }
